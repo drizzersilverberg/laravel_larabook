20c54fedb0d3429e93aa36d8c6d4caa122b970e2
Dendi Handian <dendihandian@gmail.com>
2017-12-13 08:12:06 +0000

    include partials.nav in layouts.default and add custom css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,15 +3,24 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Larabook</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <style type="text/css">
+    body {
+      padding-top: 70px;
+    }
+  </style>
 </head>
   <body>
+
+    @include('layouts.partials.nav')
+
     <div class="container">
       @yield('content')
     </div>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
   </body>
 </html>

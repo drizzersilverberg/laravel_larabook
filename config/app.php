ea406da985246c2d0271d75434db7875152036fd
Dendi Handian <dendihandian@gmail.com>
2017-12-13 03:33:43 +0000

    add laracasts/commander

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laracasts\Commander\CommanderServiceProvider::class,
     ],
 
     /*

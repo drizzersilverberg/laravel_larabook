3628576f1b54457157d0b8f19720d61ec50dee89
Dendi Handian <dendihandian@gmail.com>
2017-12-13 09:11:42 +0000

    fix pages.home view

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
   <div class="jumbotron">
-    <h1 class="display-3">Welcome to Larabook</h1>
-    <p class="lead">Welcome to the premier place to talk about Laravel with others. Why don't you sign up to see what all the fuss is about? </p>
+    <h1>Welcome to Larabook</h1>
+    <p>Welcome to the premier place to talk about Laravel with others. Why don't you sign up to see what all the fuss is about? </p>
     <p>
-      <a class="btn btn-lg btn-primary" href="#">Sign Up!</a>
+      <a class="btn btn-lg btn-primary" href="#" role="button">Sign Up!</a>
     </p>
   </div>
 @endsection

a0b3ed6618128df4e6d5d3c72255a008ebd56181
Dendi Handian <dendihandian@gmail.com>
2017-12-13 08:12:51 +0000

    use jumbotron in pages.home view

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.default')
 
 @section('content')
-    Welcome to larabook!
+  <div class="jumbotron">
+    <h1 class="display-3">Welcome to Larabook</h1>
+    <p class="lead">Welcome to the premier place to talk about Laravel with others. Why don't you sign up to see what all the fuss is about? </p>
+    <p>
+      <a class="btn btn-lg btn-primary" href="#">Sign Up!</a>
+    </p>
+  </div>
 @endsection

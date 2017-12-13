d78e9f78101bc8cb2eafd12fcb2cf4fd6e67daee
Dendi Handian <dendihandian@gmail.com>
2017-12-13 04:10:50 +0000

    add layouts.default and pages.home views

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Document</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+</head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+  </body>
+</html>

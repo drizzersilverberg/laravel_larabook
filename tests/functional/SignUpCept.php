b7c16127eafe664f36323bab237faa906206c5aa
Dendi Handian <dendihandian@gmail.com>
2017-12-13 10:35:07 +0000

    add SignUpCept functional test

---
+++ tests/functional/SignUpCept.php
@@ -0,0 +1,27 @@
+<?php
+$I = new FunctionalTester($scenario);
+
+$I->am('a guest');
+$I->wantTo('sign up for a Larabook account');
+
+$I->amOnPage('/');
+$I->click('Sign Up!');
+$I->seeCurrentUrlEquals('/register');
+
+$I->fillField('username', 'JohnDoe');
+$I->fillField('email', 'john@example.com');
+$I->fillField('password', 'demo');
+$I->fillField('password_confirmation', 'demo');
+$I->click('Sign Up');
+
+$I->seeCurrentUrlEquals('');
+$I->see('Welcome to Larabook');
+
+/*
+$I->seeRecord('users', [
+    'username' => 'JohnDoe',
+    'email' => 'john@example.com',
+])
+ */
+
+# run this test by 'vendor/bin/codecept run functional'

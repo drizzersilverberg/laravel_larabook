b85f0629752b26e6ac366f16829bc066331fbd39
Dendi Handian <dendihandian@gmail.com>
2017-12-14 05:31:14 +0000

    include layouts.partials.errors view

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -1,36 +1,45 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Register!</h1>
-    {{ Form::open(['route' => 'register_path']) }}
-        <!-- Username Form Input -->
-        <div class="form-group">
-            {{ Form::label('username', 'Username:') }}
-            {{ Form::text('username', null, ['class' => 'form-control']) }}
-        </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Register!</h1>
 
-        <!-- Email Form Input -->
-        <div class="form-group">
-            {{ Form::label('email', 'Email:') }}
-            {{ Form::text('email', null, ['class' => 'form-control']) }}
-        </div>
+            @if ($errors->any())
+                @include('layouts.partials.errors')
+            @endif
 
-        <!-- Password Form Input -->
-        <div class="form-group">
-            {{ Form::label('password', 'Password:') }}
-            {{ Form::text('password', null, ['class' => 'form-control']) }}
-        </div>
+            {{ Form::open(['route' => 'register_path']) }}
+                <!-- Username Form Input -->
+                <div class="form-group">
+                    {{ Form::label('username', 'Username:') }}
+                    {{ Form::text('username', null, ['class' => 'form-control']) }}
+                </div>
 
-        <!-- Password Confirmation Form Input -->
-        <div class="form-group">
-            {{ Form::label('password_confirmation', 'Password Confirmation:') }}
-            {{ Form::text('password_confirmation', null, ['class' => 'form-control']) }}
-        </div>
+                <!-- Email Form Input -->
+                <div class="form-group">
+                    {{ Form::label('email', 'Email:') }}
+                    {{ Form::text('email', null, ['class' => 'form-control']) }}
+                </div>
 
-        <!-- Form Submit Button -->
-        <div class="form-group">
-            {{ Form::submit('Sign Up', ['class' => 'btn btn-primary']) }}
-        </div>
+                <!-- Password Form Input -->
+                <div class="form-group">
+                    {{ Form::label('password', 'Password:') }}
+                    {{ Form::password('password', ['class' => 'form-control']) }}
+                </div>
 
-    {{ Form::close() }}
+                <!-- Password Confirmation Form Input -->
+                <div class="form-group">
+                    {{ Form::label('password_confirmation', 'Password Confirmation:') }}
+                    {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+                </div>
+
+                <!-- Form Submit Button -->
+                <div class="form-group">
+                    {{ Form::submit('Sign Up', ['class' => 'btn btn-primary']) }}
+                </div>
+
+            {{ Form::close() }}
+        </div>
+    </div>
 @endsection
